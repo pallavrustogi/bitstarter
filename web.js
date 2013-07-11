90e7a65a7f216b645e91c05c139920e0f2feae21
pallavrustogi <pallav.rustogi@gmail.com>
2013-07-11 07:36:40 +0000

    Modified as per step 6

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs =require('fs');
 var app = express.createServer(express.logger());
-
- fs.readFile('index.html', function(err, data) {
+var buffer;
+fs.readFile('index.html', function(err, data) {
         if(err) throw err;
-    var buffer = new Buffer(data,"utf-8");});
+    buffer = new Buffer(data,"utf-8");});
 
 app.get('/', function(request, response) {
     response.send(buffer.toString);
