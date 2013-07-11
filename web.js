7702258dda79aa1c5e1f28a8163a1758d573571c
pallavrustogi <pallav.rustogi@gmail.com>
2013-07-11 07:34:30 +0000

    Modified as per step 6

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 var fs =require('fs');
 var app = express.createServer(express.logger());
 
+ fs.readFile('index.html', function(err, data) {
+        if(err) throw err;
+    var buffer = new Buffer(data,"utf-8");});
+
 app.get('/', function(request, response) {
-    fs.readFile('index.html', function(err, data) {
-	if(err) throw err;
-    var buffer = new Buffer(data,"utf-8");
-    response.send(buffer.toString);});
-});
+    response.send(buffer.toString);
+    });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
